37fc4e5c02233746ea162b62b4f1a6a605f85598
saloni911 <nachankarsaloni91@gmail.com>
2024-10-22 00:12:12 +0530

    restore index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';  // Updated import for React 18
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// Create a root using the new createRoot API
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Render your App component
-root.render(
-  <StrictMode>
-    <App className='app' />
-  </StrictMode>
-);
-
-// Optionally register the service worker
+ReactDOM.render(<App class='app'></App>, document.getElementById('root'));
 serviceWorker.unregister();
